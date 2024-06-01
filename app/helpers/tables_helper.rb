98ec083aaac6a03abccaf740b1068f049671b17c
thaibhoang <hoangthai.ftu53@gmail.com>
2024-06-02 00:32:28 +0700

    Add more table instructions for business user

--- app/helpers/tables_helper.rb
+++ app/helpers/tables_helper.rb
@@ -1,2 +1,14 @@
 module TablesHelper
+  def table_size(number_of_seats)
+    case number_of_seats
+    when 1..2
+      "small"
+    when 3..5
+      "medium"
+    when 6..8
+      "large"
+    else
+      "xlarge"
+    end
+  end
 end
