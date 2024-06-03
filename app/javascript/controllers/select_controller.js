9509b7974c6be94b32615f5200061d6ca4a38c39
thaibhoang <hoangthai.ftu53@gmail.com>
2024-06-03 23:30:48 +0700

    Refactor the stimulus js file for better reading

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -3,9 +3,18 @@ import { get } from "@rails/request.js"
 
 export default class extends Controller {
   static targets = ["date", "hour", "partySize", "business", "time"]
+  static values = {
+    url: String
+  }
   change() {
-    let target = this.timeTarget.id
-    get(`/reservations/available?target=${target}&business_id=${this.businessTarget.value}&party_size=${this.partySizeTarget.value}&date=${this.dateTarget.value}&hour=${this.hourTarget.value}`, {
+    let params = new URLSearchParams();
+    params.append("target", this.timeTarget.id);
+    params.append("business_id", this.businessTarget.value);
+    params.append("party_size", this.partySizeTarget.value);
+    params.append("date", this.dateTarget.value);
+    params.append("hour", this.hourTarget.value);
+
+    get(`${this.urlValue}?${params}`, {
       responseKind: "turbo-stream"
     }); 
   }
